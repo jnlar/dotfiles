094a76100be548476d0b766ddb82217da83b9e15
jnlar <jon357@pm.me>
2024-09-02 20:16:49 +1000

    updated keymap to use new which-key syntax

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,16 +1,16 @@
 -- local, which-key keymaps
 local wk = require("which-key")
 
-wk.register({
-	["<leader>l"] = { "<cmd>Lazy<cr>", "Open lazy plugin manager" },
+wk.add({
+	{ "<leader>l", "<cmd>Lazy<cr>", desc = "Open lazy plugin manager" },
 })
 
 -- group example
-wk.register({
-	f = {
-		name = "Foo",
-		f = { "<cmd>foo<cr>", "Foo" },
-		c = { function() print("Bar") end, "Bar" }
-	}
-}, { prefix = "<leader>" })
+-- wk.register({
+-- 	f = {
+-- 		name = "Foo",
+-- 		f = { "<cmd>foo<cr>", "Foo" },
+-- 		c = { function() print("Bar") end, "Bar" }
+-- 	}
+-- }, { prefix = "<leader>" })
 
